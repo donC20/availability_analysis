1a502a2af85c71470c2243104f69d558a96f6a7e
Don Benny <donbenny916@gmail.com>
2023-09-30 01:40:11 +0530

    new changes

--- app.py
+++ app.py
@@ -1,52 +1,48 @@
 from flask import Flask, jsonify, request
 from prophet import Prophet
 import pandas as pd
+import pickle
 from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 # Load the model
-model = Prophet()
-df = pd.read_csv('availability_dataset_one_month.csv') # Adjust the path
-df['ds'] = pd.to_datetime(df['date'] + ' ' + df['start_time'])
-df['y'] = (df['availability'] == 'available').astype(int)
-model.fit(df[['ds', 'y']])
+with open('prophet_model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         data = request.get_json()
-        date = data['date']
     elif request.method == 'GET':
-        date = request.args.get('date')
+        # You can handle the GET request here if needed
+        pass
     else:
         return jsonify({"error": "Invalid request method"}), 405
-
-    # Convert date to datetime object
-    date = datetime.strptime(date, '%Y-%m-%d')
-
-    # Define time intervals
+    
+    unique_dates = pd.date_range(start=datetime.now(), periods=7)
     time_intervals = ['morning', 'forenoon', 'afternoon', 'evening', 'night']
-
-    # Generate a list of unique dates for the next 7 days
-    next_7_days = [date + timedelta(days=i) for i in range(7)]
-
-    # Initialize an empty list to store predictions
     predictions = []
 
-    for next_date in next_7_days:
+    for date in unique_dates:
         for time_interval in time_intervals:
-            time_df = df[df['time'] == time_interval]
-            start_time = pd.to_datetime(time_df['start_time'].iloc[0], format='%I:%M %p')
-            end_time = pd.to_datetime(time_df['end_time'].iloc[0], format='%I:%M %p')
-            future_start = next_date.replace(hour=start_time.hour, minute=start_time.minute)
-            future_end = next_date.replace(hour=end_time.hour, minute=end_time.minute)
+            time_data = [entry for entry in data if entry['time'] == time_interval]
+            if not time_data:
+                continue
+            start_time = pd.to_datetime(time_data[0]['start_time'], format='%I:%M %p')
+            end_time = pd.to_datetime(time_data[0]['end_time'], format='%I:%M %p')
+            future_start = date.replace(hour=start_time.hour, minute=start_time.minute)
+            future_end = date.replace(hour=end_time.hour, minute=end_time.minute)
+
             future = pd.DataFrame({'ds': [future_start, future_end]})
+            availability = 1 if time_data[0]['availability'] == 'available' else 0
+            future['availability'] = availability
+
             forecast = model.predict(future)
-            availability = 1 if (forecast['yhat'].values[0] + forecast['yhat'].values[1]) / 2 > 0.5 else 0
+            availability = 1 if forecast['yhat'].mean() > 0.5 else 0
 
             result = {
-                "date": next_date.date(),
+                "date": date.date(),
                 "time_interval": time_interval,
                 "availability": "Available" if availability == 1 else "Unavailable"
             }
--- model.py
+++ model.py
@@ -5,50 +5,52 @@ import logging
 import warnings
 import pickle
 import matplotlib.pyplot as plt
-from datetime import datetime,timedelta
+from datetime import datetime, timedelta
+
 logging.getLogger('prophet').setLevel(logging.ERROR)
 warnings.filterwarnings("ignore")
 
-# 
-
-# from google.colab import drive
-# drive.mount('/content/drive/')
-
-df = pd.read_csv('availability_dataset_one_month.csv')
-df.dropna(inplace= True)
+df = pd.read_csv('availability_dataset_one_week.csv')
+df.dropna(inplace=True)
 df.reset_index(drop=True, inplace=True)
-
-df.info()
-
-df.head()
-
-# Assuming df is your DataFrame with the relevant columns
 
 # Data Wrangling
 df['ds'] = pd.to_datetime(df['date'] + ' ' + df['start_time'])
 df['y'] = (df['availability'] == 'available').astype(int)
+df['day_of_week'] = df['ds'].dt.day_name()
 
 # Train the Prophet Model
 model = Prophet()
-model.fit(df[['ds', 'y']])
+model.add_seasonality(name='custom', period=7, fourier_order=3)  # Add a custom weekly seasonality
+model.fit(df[['ds', 'day_of_week', 'start_time', 'end_time', 'time', 'y']])
 
 # Generate a list of unique dates for the next 7 days
-unique_dates = pd.date_range(start=df['ds'].max() + pd.DateOffset(days=1), periods=7)
+unique_dates = pd.date_range(start=datetime.now(), periods=7)
 
 # Generate a list of unique time intervals
 time_intervals = df['time'].unique()
 
-# Generate predictions for each combination of date and time interval
+# Generate predictions for each combination of date, time interval, and day of the week
 for date in unique_dates:
+    print(f"Predictions for {date.date()}:")
     for time_interval in time_intervals:
-        time_df = df[df['time'] == time_interval]
-        start_time = pd.to_datetime(time_df['start_time'].iloc[0], format='%I:%M %p')
-        end_time = pd.to_datetime(time_df['end_time'].iloc[0], format='%I:%M %p')
-        future_start = date.replace(hour=start_time.hour, minute=start_time.minute)
-        future_end = date.replace(hour=end_time.hour, minute=end_time.minute)
-        future = pd.DataFrame({'ds': [future_start, future_end]})
-        forecast = model.predict(future)
-        availability = 1 if (forecast['yhat'].values[0] + forecast['yhat'].values[1]) / 2 > 0.5 else 0
-        print(f"On  {date.date()}, Time Interval: {time_interval}, Availability: {'Available' if availability == 1 else 'Unavailable'}")
-# plot_plotly(model ,forecast)
-# plot_components_plotly(model, forecast)
+        for day_of_week in df['day_of_week'].unique():
+            time_df = df[(df['time'] == time_interval) & (df['day_of_week'] == day_of_week)]
+            start_time = pd.to_datetime(time_df['start_time'].iloc[0], format='%I:%M %p')
+            end_time = pd.to_datetime(time_df['end_time'].iloc[0], format='%I:%M %p')
+            future_start = date.replace(hour=start_time.hour, minute=start_time.minute)
+            future_end = date.replace(hour=end_time.hour, minute=end_time.minute)
+            availability= (df['availability'] == 'available').astype(int)
+            future = pd.DataFrame({'ds': [future_start, future_end]})
+            future['day_of_week'] = day_of_week
+            future['start_time'] = start_time
+            future['end_time'] = end_time
+            future['time'] = time_interval
+            future['availability'] = availability
+
+            forecast = model.predict(future)
+            availability = 1 if (forecast['yhat'].values[0] + forecast['yhat'].values[1]) / 2 > 0.5 else 0
+            print(f"   - Time Interval: {time_interval}, Day of the Week: {day_of_week}, Availability: {'Available' if availability == 1 else 'Unavailable'}")
+    print()  # Add an empty line for better separation between days
+# with open('prophet_model.pkl', 'wb') as f:
+#     pickle.dump(model, f)
